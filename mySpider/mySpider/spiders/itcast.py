5b9dd60ec1bee2888fd75c972cd4cf47c2c48d85
YGM <YGM@mail.com>
2019-10-06 21:01:26 +0800

    conf spider

--- mySpider/mySpider/spiders/itcast.py
+++ mySpider/mySpider/spiders/itcast.py
@@ -5,7 +5,8 @@ import scrapy
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
     allowed_domains = ['itcast.cn']
-    start_urls = ['http://itcast.cn/']
+    start_urls = ["http://www.itcast.cn/channel/teacher.shtml",]
 
     def parse(self, response):
-        pass
+        filename = "teacher.html"
+    	open(filename, 'w').write(response.body)
