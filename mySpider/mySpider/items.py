02bae6f08201b22d4ebf43d1f248b1def736b019
UbuntuX64 <ygm@UbuntuX64.com>
2019-10-06 12:53:45 +0000

    genarate a spider

--- mySpider/mySpider/items.py
+++ mySpider/mySpider/items.py
@@ -11,4 +11,6 @@ import scrapy
 class MyspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    title = scrapy.Field()
+    info = scrapy.Field()
